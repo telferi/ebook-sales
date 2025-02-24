bd12cbc87fb6dea838edfe731ff62bb55674bedb
telferi <telferenc@gmail.com>
2025-02-24 10:23:36 +0100

    Awp-admin letiltása a bejelentkezésnél.

--- includes/class-user-subscription-login.php
+++ includes/class-user-subscription-login.php
@@ -7,15 +7,22 @@ if (!defined('ABSPATH')) {
 class User_Subscription_Login {
     
     public function __construct() {
+        // Rewrite szabály a saját login oldalhoz
         add_action('init', [$this, 'add_rewrite_rules']);
         add_filter('query_vars', [$this, 'add_query_vars']);
         add_action('template_redirect', [$this, 'template_redirect']);
+        
+        // wp-login.php letiltása és átirányítása
+        add_action('login_init', [$this, 'disable_wp_login']);
+
+        // Módosítjuk a login_url generálódását is
+        add_filter('login_url', [$this, 'filter_login_url'], 10, 3);
     }
     
     // Rewrite szabály hozzáadása a login oldalhoz
     public function add_rewrite_rules() {
         $login_slug = get_option('user_subscription_login_page', 'login');
-        add_rewrite_rule('^' . $login_slug . '/?$', 'index.php?user_subscription_login=1', 'top');
+        add_rewrite_rule('^' . preg_quote($login_slug, '/') . '/?$', 'index.php?user_subscription_login=1', 'top');
     }
     
     // Egyéni query var engedélyezése
@@ -40,7 +47,7 @@ class User_Subscription_Login {
         }
         
         $errors = [];
-        // űrlap feldolgozása
+        // Űrlap feldolgozása
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['user_login'])) {
             if (!isset($_POST['user_subscription_login_nonce']) || 
                 !wp_verify_nonce($_POST['user_subscription_login_nonce'], 'user_subscription_login_action')) {
@@ -61,7 +68,6 @@ class User_Subscription_Login {
             }
         }
         
-        // Oldal fejléc betöltése
         get_header();
         echo '<div class="user-login">';
         echo '<h1>' . __('Bejelentkezés', 'ebook-sales') . '</h1>';
@@ -92,8 +98,25 @@ class User_Subscription_Login {
         </form>
         <?php
         echo '</div>';
-        // Oldal lábléc betöltése
         get_footer();
+    }
+    
+    // wp-login.php letiltása: átirányítás a saját login oldalra
+    public function disable_wp_login() {
+        // DOING_AJAX esetén ne irányítsuk át
+        if (defined('DOING_AJAX') && DOING_AJAX) {
+            return;
+        }
+        // Átirányítás a saját login oldalra
+        $login_slug = get_option('user_subscription_login_page', 'login');
+        wp_redirect(home_url('/' . $login_slug));
+        exit;
+    }
+    
+    // Módosítja a login_url szűrőn keresztül az URL-t a saját login oldalra
+    public function filter_login_url($login_url, $redirect, $force_reauth) {
+        $login_slug = get_option('user_subscription_login_page', 'login');
+        return home_url('/' . $login_slug);
     }
 }
 
