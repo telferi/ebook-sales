abca42a53db0c8a2626f65496a2e17b3e94c57d6
telferi <telferenc@gmail.com>
2025-03-26 21:07:48 +0100

    Uj poszt boritókép beszúrása kozepre,

--- ebook-sales.php
+++ ebook-sales.php
@@ -42,7 +42,7 @@ require_once EBOOK_SALES_PLUGIN_DIR . 'includes/ebook-featured-image.php';
 require_once EBOOK_SALES_PLUGIN_DIR . 'includes/handle-ai-generate.php';
 require_once EBOOK_SALES_PLUGIN_DIR . 'includes/wp-kses-config.php';
 require_once EBOOK_SALES_PLUGIN_DIR . 'includes/save-ai-meta.php';
-require_once EBOOK_SALES_PLUGIN_DIR . 'includes/insert-ebook-cover.php';
+// require_once EBOOK_SALES_PLUGIN_DIR . 'includes/insert-ebook-cover.php';
 require_once EBOOK_SALES_PLUGIN_DIR . 'includes/meta-box-cover-image.php';
 
 // Új AI setup osztály betöltése
