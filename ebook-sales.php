4b0de702cf1d1618e9f54fd42979daecb65f58ba
telferi <telferenc@gmail.com>
2025-03-27 10:46:49 +0100

    Uj poszt gomb létrehozása01.

--- ebook-sales.php
+++ ebook-sales.php
@@ -45,6 +45,7 @@ require_once EBOOK_SALES_PLUGIN_DIR . 'includes/save-ai-meta.php';
 // require_once EBOOK_SALES_PLUGIN_DIR . 'includes/insert-ebook-cover.php';
 require_once EBOOK_SALES_PLUGIN_DIR . 'includes/meta-box-cover-image.php';
 require_once EBOOK_SALES_PLUGIN_DIR . 'includes/insert-ebook-cover-frontend.php';
+require_once EBOOK_SALES_PLUGIN_DIR . 'includes/ebook-pay-button.php';
 
 // Új AI setup osztály betöltése
 require_once EBOOK_SALES_PLUGIN_DIR . 'includes/class-ai-setup.php';
